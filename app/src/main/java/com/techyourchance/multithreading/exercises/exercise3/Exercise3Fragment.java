f45c6ac23e5476db2d5ae1fd7b31f33ac4e9b3e6
Pshypher <bshote@getcarbon.co>
2025-01-29 05:04:57 +0100

    Completes the third exercise

--- app/src/main/java/com/techyourchance/multithreading/exercises/exercise3/Exercise3Fragment.java
+++ app/src/main/java/com/techyourchance/multithreading/exercises/exercise3/Exercise3Fragment.java
@@ -1,6 +1,8 @@
 package com.techyourchance.multithreading.exercises.exercise3;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +16,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.Locale;
+
 public class Exercise3Fragment extends BaseFragment {
 
     private static final int SECONDS_TO_COUNT = 3;
@@ -24,6 +28,8 @@ public class Exercise3Fragment extends BaseFragment {
 
     private Button mBtnCountSeconds;
     private TextView mTxtCount;
+
+    private final Handler mUiHandler = new Handler(Looper.getMainLooper());
 
     @Nullable
     @Override
@@ -55,5 +61,25 @@ public class Exercise3Fragment extends BaseFragment {
         3. Show count in TextView
         4. When count completes, show "done" in TextView and enable the button
          */
+        mBtnCountSeconds.setEnabled(false);
+        Thread thread = new Thread(() -> {
+            for (int i = 0; i <= SECONDS_TO_COUNT; i++) {
+                try {
+                    Thread.sleep(1000);
+                    final int finalCount = i + 1;
+                    mUiHandler.post(() -> {
+                        mTxtCount.setText(String.format(Locale.getDefault(), "%d", finalCount));
+                    });
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            mUiHandler.post(() -> {
+                mTxtCount.setText(getString(R.string.msg_done));
+                mBtnCountSeconds.setEnabled(true);
+            });
+        });
+        thread.start();
     }
 }
